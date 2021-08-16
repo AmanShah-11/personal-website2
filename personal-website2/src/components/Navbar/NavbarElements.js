a9e2b054d47e3e206f19b7ed18f2b89173064e53
Aman Shah <a232shah@uwaterloo.ca>
2021-08-15 21:26:28 -0400

    Added sign in button

--- personal-website2/src/components/Navbar/NavbarElements.js
+++ personal-website2/src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
 import { Link as LinkR } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
+import styled from "styled-components";
 
 export const Nav = styled.nav`
   background: #000;
@@ -29,7 +29,7 @@ export const NavbarContainer = styled.div`
 `;
 
 export const NavLogo = styled(LinkR)`
-  color: red;
+  color: #fff;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
@@ -76,11 +76,40 @@ export const NavLinks = styled(LinkS)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1 rem;
+  padding: 0 1rem;
   height: 100%;
   cursor: pointer;
 
   &.active {
     border-bottom: 3px solod #01bf71;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out
+  text=decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
   }
 `;
