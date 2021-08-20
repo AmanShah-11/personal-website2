988cccf91b79f50edde16ae6013b122f5b0fe8d0
Aman Shah <a232shah@uwaterloo.ca>
2021-08-20 15:12:49 -0400

    Finished Info Section

---
+++ personal-website2/src/components/InfoSection/Data.js
@@ -0,0 +1,57 @@
+import img1 from "../../images/svg-1.svg";
+import img2 from "../../images/svg-2.svg";
+import img3 from "../../images/svg-3.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: img1,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: img2,
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: img3,
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
