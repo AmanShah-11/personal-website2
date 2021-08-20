3c8237e8c622c29075df5b11d49f6d85427b090f
Aman Shah <a232shah@uwaterloo.ca>
2021-08-20 13:53:27 -0400

    Added content to info

--- personal-website2/src/pages/index.js
+++ personal-website2/src/pages/index.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +16,7 @@ const Home = () => {
       {/* <Sidebar isOpen={isOpen} toggle={toggle} /> */}
       <Navbar toggle={toggle} />
       <HeroSection />
-      <InfoSection />
+      <InfoSection {...homeObjOne} />
     </>
   );
 };
