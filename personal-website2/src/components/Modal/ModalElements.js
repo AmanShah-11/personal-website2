3493b0f8fed1dcff7626f1a4438ccac49bb3ec0d
Aman Shah <a232shah@uwaterloo.ca>
2021-08-23 01:39:14 -0400

    Added custom styling for modal

---
+++ personal-website2/src/components/Modal/ModalElements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Modal } from "react-bootstrap";
+
+export const CustomModalBody = styled(Modal.Body)`
+  white-space: pre-wrap;
+  padding: 1rem;
+  display: block;
+  text-indent: 0px;
+`;
